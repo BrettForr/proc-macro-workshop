b38751621201576947906bba030b70b59c34e824
Brett Forrest <brettforrest@Bretts-MacBook-Pro.local>
2025-01-26 19:05:01 -0600

    Builder 04 done

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -17,12 +17,20 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let builder_setters = generate_builder_setters(&parsed_input.data, &name);
 
+    let set_expressions = set_fields(&parsed_input.data, &name);
+
     let expanded = quote! {
         pub struct #builder_name {
             #builder_fields
         }
 
         impl #builder_name {
+            pub fn build(self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(#name {
+                    #set_expressions
+                })
+            }
+
             #builder_setters
         }
 
@@ -113,7 +121,7 @@ fn generate_builder_setters(data: &Data, ident: &proc_macro2::Ident) -> proc_mac
                     let name_unwrapped = name.as_ref();
 
                     if let Some(n) = name_unwrapped {
-                        quote! { fn #n(&mut self, #n: #field_type) -> &mut Self {
+                        quote! { pub fn #n(&mut self, #n: #field_type) -> &mut Self {
                             self.#n = Some(#n);
                             self
                         }}
@@ -125,6 +133,42 @@ fn generate_builder_setters(data: &Data, ident: &proc_macro2::Ident) -> proc_mac
 
                 quote! {
                     #(#fields_setters)*
+                }
+            }
+            _ => Error::new_spanned(ident, "Expected Struct with Named Fields").to_compile_error(),
+        },
+        _ => Error::new_spanned(ident, "Expected Struct with Named Fields").to_compile_error(),
+    };
+
+    expanded
+}
+
+fn set_fields(data: &Data, ident: &proc_macro2::Ident) -> proc_macro2::TokenStream {
+    let expanded = match *data {
+        Data::Struct(ref data_struct) => match data_struct.fields {
+            Fields::Named(ref fields_named) => {
+                let set_expressions = fields_named.named.iter().map(|field| {
+                    let name = &field.ident;
+
+                    let name_unwrapped = name.as_ref();
+
+                    if let Some(n) = name_unwrapped {
+                        let name_value = format_ident!("{}_value", n);
+                        quote! {
+                            #n: {if let Some(#name_value) = self.#n {
+                                #name_value
+                            } else {
+                                return Err(format!("Field {} is not set", stringify!(#n)).into());
+                            }}
+                        }
+                    } else {
+                        Error::new_spanned(ident, "Expected Struct with Named Fields")
+                            .to_compile_error()
+                    }
+                });
+
+                quote! {
+                    #(#set_expressions,)*
                 }
             }
             _ => Error::new_spanned(ident, "Expected Struct with Named Fields").to_compile_error(),
