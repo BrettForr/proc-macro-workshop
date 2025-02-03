b08383d12ed66115abf7e28bb08ac98bb8a9edfd
Brett Forrest <brettforrest@Bretts-MacBook-Pro.local>
2025-02-03 13:33:08 -0800

    Sorted 02 done

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,14 +1,29 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Item};
+use syn::{Item, ItemEnum};
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let parsed_input = parse_macro_input!(input as Item);
+pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let parsed_item_enum = parse_enum(input);
 
-    let expanded = quote! {
-        #parsed_input
+    let expanded = match parsed_item_enum {
+        Ok(item_enum) => quote! {
+            #item_enum
+        },
+        Err(err) => err.to_compile_error(),
     };
 
     TokenStream::from(expanded)
+}
+
+fn parse_enum(input: TokenStream) -> Result<ItemEnum, syn::Error> {
+    let parsed_input = syn::parse::<Item>(input)?;
+
+    match parsed_input {
+        Item::Enum(item_enum) => Ok(item_enum),
+        _ => Err(syn::Error::new(
+            proc_macro2::Span::call_site(),
+            "expected enum or match expression",
+        )),
+    }
 }
